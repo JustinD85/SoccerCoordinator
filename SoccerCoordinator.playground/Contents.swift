6dbabcf4e688f10022e55ae2ec1e969a4adec63f
ibdv4521 <justin@bluedraft.studio>
2017-03-09 20:17:06 +0900

    18 Dictionaries of players made.

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -1,7 +1,37 @@
-//: Playground - noun: a place where people can play
 
-import UIKit
+var joeSmith: [String: String] = ["fName": "Joe", "lName": "Smith","Height": "42","Soccer Experience": "YES","Guardian Names": "Jim and Jan Smith"]
 
-var str = "Hello, playground"
+var jillTanner: [String: String] = ["fName": "Jill", "lName": "Tanner","Height": "36","Soccer Experience": "YES","Guardian Names": "Clara Tanner"]
 
-// Justin's Comment
+var billBon: [String: String] = ["fName": "Bill", "lName": "Bon","Height": "43","Soccer Experience": "YES","Guardian Names": "Sara and Jenny Bon"]
+
+var evaGordon: [String: String] = ["fName": "Eva", "lName": "Gordon","Height": "45","Soccer Experience": "NO","Guardian Names": "Wendy and Mike Gordon"]
+
+var mattGill: [String: String] = ["fName": "Matt", "lName": "Gill","Height": "40","Soccer Experience": "NO","Guardian Names": "JCharles and Sylvia Gill"]
+
+var kimmyStein: [String: String] = ["fName": "Kimmy", "lName": "Stein","Height": "41","Soccer Experience": "NO","Guardian Names": "Bill and Hillary Stein"]
+
+var sammyAdams: [String: String] = ["fName": "Sammy", "lName": "Adams","Height": "45","Soccer Experience": "NO","Guardian Names": "Jeff Adams"]
+
+var karlSaygan: [String: String] = ["fName": "Karl", "lName": "Saygan","Height": "42","Soccer Experience": "YES","Guardian Names": "Heather Bledsoe"]
+
+var suzaneGreenberg: [String: String] = ["fName": "Suzane", "lName": "Greenberg","Height": "44","Soccer Experience": "YES","Guardian Names": "Henrietta Dumas"]
+
+var salDali: [String: String] = ["fName": "Sal", "lName": "Dali","Height": "41","Soccer Experience": "NO","Guardian Names": "Gala Dali"]
+
+var joeKavalier: [String: String] = ["fName": "Joe", "lName": "Kavalier","Height": "39","Soccer Experience": "NO","Guardian Names": "Sam and Elaine Kavalier"]
+
+var benFinkelstein: [String: String] = ["fName": "Ben", "lName": "Finklestein","Height": "44","Soccer Experience": "NO","Guardian Names": "Aaron and Jill Finkelstein"]
+
+var diegoSoto: [String: String] = ["fName": "Diego", "lName": "Soto","Height": "41","Soccer Experience": "YES","Guardian Names": "Robin and Sarika Soto"]
+
+var chloeAlaska: [String: String] = ["fName": "Chloe", "lName": "Alaska","Height": "47","Soccer Experience": "NO","Guardian Names": "David and Jamie Alaska"]
+
+var arnoldWillis: [String: String] = ["fName": "Arnold", "lName": "Willis","Height": "43","Soccer Experience": "NO","Guardian Names": "Claire Willis"]
+
+var phillipHelm: [String: String] = ["fName": "Phillip", "lName": "Helm","Height": "44","Soccer Experience": "YES","Guardian Names": "Thomas Helm and Eva Jones"]
+
+var lesClay: [String: String] = ["fName": "Les", "lName": "Clay","Height": "42","Soccer Experience": "YES","Guarian Names": "Wynonna Brown"]
+
+var herschelKrustofski: [String: String] = ["fName": "Herschel", "lName": "Krustofski","Height": "45","Soccer Experience": "YES","Guardian Names": "Hyman and Rachel Krustofski"]
+
